f393a65272d5fd158916b32eb2c56b5506801f37
sasa04 <timsitmelosa@gmail.com>
2024-12-30 19:54:44 +0200

    book class and user

--- src/Books.py
+++ src/Books.py
@@ -1,16 +1,38 @@
 class Books:
-    def __init__(self, title, author, year, genre, T_copies,Ava_copies):
+    #this is a factory class
+    def __init__(self, title, author, year, genre, T_copies, Ava_copies):
         self.title = title
-        self.author =author
+        self.author = author
         self.year = year
         self.genre = genre
         self.T_copies = T_copies
         self.Ava_copies = Ava_copies
-    def borrow_book(self):
-        if self.Ava_copies != 0:
-            self.Ava_copies -=1
-        else:
-            print("No availible books left to remove")
-            self.Ava_copies=0
-    def return_book(self):
+
+    def add_to_library(self):
+        self.T_copies += 1
         self.Ava_copies +=1
+
+    def remove_from_library(self):
+        if self.T_copies > 0:
+            self.T_copies -= 1
+            if self.Ava_copies > 0:
+                self.Ava_copies-=1
+        else:
+            print("No copies left to remove.")
+
+    def borrow_book(self):
+        if self.Ava_copies > 0:
+            self.Ava_copies -= 1
+        else:
+            print("No available books left to remove")
+
+    def return_book(self):
+        if self.Ava_copies < self.T_copies:
+            self.Ava_copies += 1
+        else:
+            print("All copies are in the library.")
+
+    def __str__(self):
+        return (f"Title: {self.title}, Author: {self.author}, Year: {self.year}, "f"Genre: {self.genre}, Total Copies: {self.T_copies}, "
+                f"Available Copies: {self.Ava_copies}")
+
